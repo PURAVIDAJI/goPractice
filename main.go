05dce3d4c4f277410a15e91e6e32fb5926221cce
PURAVIDAJI <ijk9501@naver.com>
2025-05-25 14:22:48 +0800

    var

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Print("Hello World")
+
+	var conferenceName = "Go Conference"
+	const conferenceTickets =50 //고정값
+	var remainingTickets =50
+
+	fmt.Printf("Welcome to %v booking application\n",conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n",conferenceTickets,remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+
+	userName = "Tom"
+	userTickets =2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+
+	
+
+
+
+
+	
+
+
 }
